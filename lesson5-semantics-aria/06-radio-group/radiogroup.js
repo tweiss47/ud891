ec269c9d75b0db2c829fead91217c03fa4e9e71e
Taylor Weiss <taylor.weiss@comcast.net>
2018-10-25 13:55:29 -0700

    Add aria labels and roles

--- lesson5-semantics-aria/06-radio-group/radiogroup.js
+++ lesson5-semantics-aria/06-radio-group/radiogroup.js
@@ -92,12 +92,14 @@
     // Set the old button to tabindex -1
     this.focusedButton.tabIndex = -1;
     this.focusedButton.removeAttribute('checked');
+    this.focusedButton.setAttribute('aria-checked', 'false');
 
     // Set the new button to tabindex 0 and focus it
     this.focusedButton = this.buttons[this.focusedIdx];
     this.focusedButton.tabIndex = 0;
     this.focusedButton.focus();
     this.focusedButton.setAttribute('checked', '');
+    this.focusedButton.setAttribute('aria-checked', 'true');
 
     // ... we probably want to do some stuff here, too ...
 
